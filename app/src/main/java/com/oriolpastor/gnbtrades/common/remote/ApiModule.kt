b262a2b835f01f09d070d0097e74af1826e6fac8
xXdasterZz <oriol13rugby@gmail.com>
2021-10-13 07:39:49 +0200

    add request interceptor

--- app/src/main/java/com/oriolpastor/gnbtrades/common/remote/ApiModule.kt
+++ app/src/main/java/com/oriolpastor/gnbtrades/common/remote/ApiModule.kt
@@ -4,6 +4,7 @@ import org.koin.dsl.module
 
 val apiModule = module {
     single { ApiProvider.provideApi(get()) }
-    single { ApiProvider.provideRetrofit(get()) }
+    single { ApiProvider.provideRetrofit(get(), get()) }
+    single { ApiProvider.provideOkHttpClient() }
     single { ApiProvider.provideMoshiConverter() }
 }
--- app/src/main/java/com/oriolpastor/gnbtrades/common/remote/ApiProvider.kt
+++ app/src/main/java/com/oriolpastor/gnbtrades/common/remote/ApiProvider.kt
@@ -2,6 +2,8 @@ package com.oriolpastor.gnbtrades.common.remote
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.component.KoinComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -11,11 +13,21 @@ object ApiProvider : KoinComponent {
     fun provideApi(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
 
     fun provideRetrofit(
-        moshiConverterFactory: MoshiConverterFactory
+        moshiConverterFactory: MoshiConverterFactory,
+        client: OkHttpClient
     ): Retrofit = Retrofit.Builder()
         .baseUrl("https://quiet-stone-2094.herokuapp.com/")
         .addConverterFactory(moshiConverterFactory)
+        .client(client)
         .build()
+
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor())
+            .addInterceptor(RequestInterceptor())
+            .build()
+
+    }
 
     fun provideMoshiConverter(): MoshiConverterFactory = MoshiConverterFactory.create(
         Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
